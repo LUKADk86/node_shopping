8c8d56b678bae0b565dad5be05ae79137f3fbfcf
LUKADk86 <lounis.86@gmail.com>
2019-10-15 09:46:29 +0200

    connexion a la db

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+mongoose.connect('mongodb://localhost/pizzaShopping',{useNewUrlParser:true, useUnifiedTopology: true},(error)=>{
+  if(error){
+    console.log(error)
+  }
+  console.log('connexion réussie à la db pizzeria')
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
