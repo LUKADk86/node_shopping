9e1f162c25e07d6ddafec425799ca03655ef9edd
LUKADk86 <lounis.86@gmail.com>
2019-10-16 21:42:07 +0200

    dropdown management

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,28 @@ var router = express.Router();
 const Product = require('../models/Product');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    Product.find({}, (error, result)=>{
-      if(error){
-        console.log(error);
-       
-      }
-      var productGrid=[];
-      var columnGrid=3;
-      for(var i=0; i<result.length; i+=columnGrid){
+router.get('/', function (req, res, next) {
 
-        productGrid.push(result.slice(i, i+columnGrid));
-      }
-      
-      res.render('index', { title: 'carte du shopping', products: productGrid });
-    })
-    
-  
-  
+  Product.find({}, (error, result) => {
+    if (error) {
+      console.log(error);
+
+    }
+    //un tableau contenant des tableau de 3 objets pour un affichage dynamique
+    var productGrid = [];
+    //constant permet de limiter 3 par ligne c a d le sous tab de 3 objet
+    var columnGrid = 3;
+    //boucle pour creer le tab productGrid
+    for (var i = 0; i < result.length; i += columnGrid) {
+
+      productGrid.push(result.slice(i, i + columnGrid));
+    }
+
+    res.render('index', { title: 'carte du shopping', products: productGrid, checkuser: req.isAuthenticated() });
+  })
+
+
+
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 const passport = require('passport');
 
 /* GET users listing. */
-router.get('/signup', function (req, res, next) {
+router.get('/signup',isNotSignin, function (req, res, next) {
   var messageError = req.flash('signupError');
   res.render('user/signup', { message: messageError });
 });
@@ -43,13 +43,14 @@ function (req, res, next) {
   failureRedirect: 'signup',
   failureFlash : true,
 }))
-router.get('/signin', (req, res, next)=>{
+router.get('/signin',isNotSignin, (req, res, next)=>{
   //passer les erreurs d'authentification difinies dans passport local-signin
   errormsg=req.flash('signinError')
   res.render('user/signin', {message: errormsg}) 
 });
-router.get('/profile', (req, res, next)=>{
-  res.render('user/profile') 
+router.get('/profile',isSignin, (req, res, next)=>{
+  //checkuser en true pour changer l'affiche de menu header
+  res.render('user/profile', {checkuser: true, checkprofile: true}) 
 });
 router.post('/signin', [
   //verifier avec express validator les checks
@@ -79,15 +80,32 @@ router.post('/signin', [
     //le next permet de passer a la 2eme fucntion callbacl passport.authenticate sinon le processus s'arrête
     next();
     }
-
-
-
-
-
 ,passport.authenticate(/*mettre le meme que celui de passportstrategy*/'local-signin', {
   //2eme parametre objet en cas de succes et le message et le redi en cas d echec selon le fichier passport
   successRedirect : 'profile',
   failureRedirect: 'signin',
   failureFlash : true,
-}) )
+}))
+//route qui permet de deconnecter (effacer) la session
+router.get('/logout', isSignin,(req,res,next)=>{
+  req.logOut();
+  res.redirect('/')
+ 
+} )
+//permet d'empêcher l'acces sans authentification a /profile, on l'utilise a get profile et get logout
+function isSignin(req, res, next){
+  if(req.isUnauthenticated()){
+    res.redirect('/users/signin')
+    return;
+  }
+  next();
+}
+//permet de redireger si l'user tente d accecer a la page connexion ou entregistrement, une fois authentifié, utilisé a get signin et get signup
+function isNotSignin(req, res, next){
+  if(req.isAuthenticated()){
+    res.redirect('/')
+    return;
+  }
+  next();
+}
 module.exports = router;
